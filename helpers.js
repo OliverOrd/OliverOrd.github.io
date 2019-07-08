8c07a0b0c0104e65c0cc95627841d11cffb03652
Oliver Ord <44840410+OliverOrd@users.noreply.github.com>
2019-07-08 23:23:35 +0100

    Add files via upload

---
+++ helpers.js
@@ -0,0 +1,98 @@
+const items = `#love
+#instagood
+#photooftheday
+#fashion
+#beautiful
+#dji
+#dronestagram
+#photography
+#dronephotography
+#djimavic
+#aerialphotography
+#sunset
+#dronesdaily
+#city
+#trip
+#photographyempire
+#lensbible
+#drone
+#dronenature
+#mavicair
+#droneoftheday
+#dronefly
+#droneislife
+#droneporn
+#art
+#visual
+#aerial
+#naturephotography
+#earth
+#dailyoverview
+#topdownphoto
+#birdseyeview
+#fromwhereidrone
+#aerialphoto
+#birdseye
+#droneglobe
+#dronephoto
+#unitedbydrone
+#iamdji
+#djicreator
+#createexplore
+#beautifuldestinations
+#dronepic
+#topdown
+#passionpassport
+#beautifulmatters
+#agameoftones
+#ig_masters
+#thecreatorclass
+#minimalism
+#geometry
+#architecture
+#doyouevendrone
+#dronelyfe
+#dronemultimedia
+#ourplanetdaily
+#roamtheplanet
+#liveoutside
+#dronepointofview
+#bestoftheday
+#wonderful_places
+#landscape
+#outdoors
+#justgoshoot
+#vacation
+#dronebois
+#droneheros
+#airvuz
+#lumenier
+#dronie
+#welltraveled
+#aerialphoto
+#helicopter
+#visiting
+#igtravel
+#arts
+#perspective
+#planetearth
+#skypixel
+#paradise
+#nature`;
+
+function copyToClipboard(text) {
+    let dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
+
+function randNum(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function getList() {
+	return items.split("\n");
+}
