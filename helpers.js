f9519d691da8ed4e074c6ac7b026f1068681174b
Oliver Ord <44840410+OliverOrd@users.noreply.github.com>
2019-07-09 21:43:53 +0100

    Added category feature, removed copy feature

--- helpers.js
+++ helpers.js
@@ -1,4 +1,5 @@
-const items = `#love
+const items = {
+	drones: `#love
 #instagood
 #photooftheday
 #fashion
@@ -91,7 +92,105 @@ const items = `#love
 #planetearth
 #skypixel
 #paradise
-#nature`;
+#nature`,
+
+	travel: `#love
+#instagood
+#photooftheday
+#fashion
+#beautiful
+#travel
+#instatravel
+#sunset
+#photographyempire
+#lensbible
+#ourplanetdaily
+#roamtheplanet
+#liveoutside
+#beach
+#ocean
+#goldenhour
+#picoftheday
+#pictureoftheday
+#bestoftheday
+#wonderful_places
+#welltravelled
+#exploreeverything
+#igmasters
+#vacation
+#welltraveled
+#helicopter
+#visiting
+#igtravel
+#tourist
+#instago
+#instapassport
+#instatravelling
+#travelingram
+#mytravelgram
+#relax
+#traveler
+#holidays
+#summertime
+#calm
+#peace
+#meditation
+#sunnysideoflife
+#paradise
+#wanderlust
+#travelling
+#traveler
+#tourism
+#traveltheworld
+#outdoors
+#lonelyplanet
+#globalcaptures
+#bestcaptureglobal
+#depthsofearth
+#gardenofthegods
+#ig_color
+#earthfocus
+#nakedplanet
+#whyweshoot
+#shotzdelight
+#beachvibes
+#vacationtravel
+#explorepage`,
+	
+	photography: `#love
+#instagood
+#photooftheday
+#fashion
+#beautiful
+#sonya6300
+#mirrorlesscamera
+#sonyhomies
+#sonyphotography
+#photographyempire
+#lensbible
+#picoftheday
+#pictureoftheday
+#justgoshoot
+#bestoftheday
+#wonderful_places
+#vacation
+#arts
+#lines
+#perspective
+#ig_color
+#earthfocus
+#nakedplanet
+#whyweshoot
+#shotzdelight
+#beachvibes
+#vacationtravel
+#explorepage
+#perspective
+#planetearth
+#skypixel`
+};
+
+
 
 function copyToClipboard(text) {
     let dummy = document.createElement("textarea");
@@ -107,5 +206,5 @@ function randNum(min, max) {
 }
 
 function getList() {
-	return items.split("\n");
+	return items;
 }
